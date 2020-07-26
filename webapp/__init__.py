e0138aefc6bf0ded2cae119046533c4d1a9c6a6d
Crowiant <ant.nitochkin@gmail.com>
2020-07-26 16:42:12 +0300

    add validating url in create_task

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 
 def create_app():
@@ -6,7 +7,18 @@ def create_app():
 
     @app.route('/', methods=['POST'])
     def create_task():
-        return 'Mock for creating task'
+        url = request.data
+
+#       Validating html TODO: make deco from this
+        try:
+            get_html = requests.get(url)
+        except (requests.exceptions.MissingSchema,
+                requests.exceptions.InvalidSchema):
+            return 'Not valid url'
+        except requests.exceptions.ConnectionError:
+            return "Can't connect to url", 500
+
+        return url
 
     @app.route('/')
     @app.route('/<int:task_id>')
